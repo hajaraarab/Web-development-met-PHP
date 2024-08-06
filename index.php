c29ff960951fb71cbad6fdf750c06c561eecb445
hajaraarab <hajar.aarab@student.kdg.be>
2024-08-06 14:44:15 +0200

    Juiste stylesheet toegevoegd

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-include './templates/allArticles.php';
+
 include './templates/navigation.php';
 include './templates/header.php';
 include './templates/main.php';
 include './templates/articles.php';
 include './templates/form.php';
+include './templates/footer.php';
 
 ?>
 <!DOCTYPE html>
@@ -13,7 +14,7 @@ include './templates/form.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./ressources/style.css" rel="stylesheet">
+    <link href="./resources/style.css" rel="stylesheet">
     <title>HAJAR AARAB</title>
 </head>
 <body>
